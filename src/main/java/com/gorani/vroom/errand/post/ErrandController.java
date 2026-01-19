c51374a7450e26ac0a17498e99207693be774c11
huni6711 <huni6711@naver.com>
2026-01-19 18:26:44 +0900

    feat: 컨트롤러에서 더미 데이터 지운 상태

--- src/main/java/com/gorani/vroom/errand/post/ErrandController.java
+++ src/main/java/com/gorani/vroom/errand/post/ErrandController.java
@@ -1,12 +1,14 @@
 package com.gorani.vroom.errand.post;
 
-import lombok.RequiredArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
@@ -40,6 +42,27 @@ public class ErrandController {
         return "errand_list";
     }
 
+//    /**
+//     * 심부름 게시글 상세
+//     */
+//    @GetMapping("/errand/detail")
+//    public String errandDetail(
+//            @RequestParam("errandsId") Long errandsId,
+//            Model model
+//    ) {
+//        ErrandDetailVO errand = errandService.getErrandDetail(errandsId);
+//
+//        if (errand == null) {
+//            // 존재하지 않는 글이면 목록으로 돌려보냄
+//            return "redirect:/errand/list";
+//        }
+//
+//        model.addAttribute("errand", errand);
+//        return "errand_detail";
+//    }
+    
+    
+    
     /**
      * 심부름 게시글 상세
      */
@@ -48,6 +71,7 @@ public class ErrandController {
             @RequestParam("errandsId") Long errandsId,
             Model model
     ) {
+    	List<ErrandListVO> nearbyErrands = new ArrayList<>();
         ErrandDetailVO errand = errandService.getErrandDetail(errandsId);
 
         if (errand == null) {
